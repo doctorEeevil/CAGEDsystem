3f28dcb1c2b4185b17fe7ee4628535d88bce8844
Samuel Aiudi-Murphy <sam@pop-os.localdomain>
2020-09-28 17:23:58 -0700

    adds a css file, roughs in our html, and adds the begginnings of our JS

--- caged.js
+++ caged.js
@@ -1,1 +1,19 @@
-alert("IT WORKED!!!!");
+window.onload = function(){
+    readdata(1);
+};
+
+function readdata(num){
+    /*
+      sticks the caged data in a string
+      note this is everything in script tag
+    */
+    var data = document.querySelector("#data");
+    /*
+      breaks apart the string into an array
+      new array items on line breaks
+    */
+    var lines = data.text.split('\n');
+    lines.pop(); // remove junk from end of array
+    lines.shift(); // remove junk on beginning of array
+    console.log(lines)
+}
